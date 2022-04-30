3e051ae43313fbc3ff8d2eda350c2ee9c4b59e91
TeNeBiT-eng <ademolababalola2003@gmail.com>
2022-04-30 17:00:07 +0100

    changed js

--- js/footer.js
+++ js/footer.js
@@ -1,2 +1,2 @@
-changeicon = (icons) => icons.classList.toggle("fa-angle-up");
-icons.addEventListener("click", () => show.classList.toggle("left"));
+// changeicon = (icons) => icons.classList.toggle("fa-angle-up");
+// icons.addEventListener("click", () => show.classList.toggle("left"));
--- js/header.js
+++ js/header.js
@@ -1,8 +1,9 @@
-// changeicon = (this) => this.classList.toggle("fa-times");
-// this.addEventListener("click", () => show.classList.toggle("left"));
-var btn = document.getElementById("language")
-var ul = document.getElementById("myDropdown")
+changeicon = (icons) => icons.classList.toggle("fa-times");
+icons.addEventListener("click", () => show.classList.toggle("left"));
 
-btn.addEventListener('click', function(){
-    ul.classList.toggle('shows')
-})
+// var btn = document.getElementById("language")
+// var ul = document.getElementById("myDropdown")
+
+// btn.addEventListener('click', function(){
+//     ul.classList.toggle('shows')
+// })
