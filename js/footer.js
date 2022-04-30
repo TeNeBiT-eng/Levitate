b64ef691d51b57d63f76946ccd69bd84da2225d1
TeNeBiT-eng <ademolababalola2003@gmail.com>
2022-04-30 14:01:04 +0100

    added font awesome icons in footer

---
+++ js/footer.js
@@ -0,0 +1,2 @@
+changeicon = (icons) => icons.classList.toggle("fa-angle-up");
+icons.addEventListener("click", () => show.classList.toggle("left"));
--- js/header.js
+++ js/header.js
@@ -1,2 +1,2 @@
 changeicon = (icons) => icons.classList.toggle("fa-times");
-icons.addEventListener("click", () => show.classList.toggle("left"));
+icons.addEventListener("click", () => show.classList.toggle("down"));
