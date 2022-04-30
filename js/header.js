20f8e2809d32bcd2f381eb23b588e3789e6a0fbc
TeNeBiT-eng <ademolababalola2003@gmail.com>
2022-04-30 16:12:53 +0100

    commit

--- js/header.js
+++ js/header.js
@@ -1,2 +1,8 @@
-changeicon = (icons) => icons.classList.toggle("fa-times");
-icons.addEventListener("click", () => show.classList.toggle("down"));
+// changeicon = (this) => this.classList.toggle("fa-times");
+// this.addEventListener("click", () => show.classList.toggle("left"));
+var btn = document.getElementById("language")
+var ul = document.getElementById("myDropdown")
+
+btn.addEventListener('click', function(){
+    ul.classList.toggle('shows')
+})
